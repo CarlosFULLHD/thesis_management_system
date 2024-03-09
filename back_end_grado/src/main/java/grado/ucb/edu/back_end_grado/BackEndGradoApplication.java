fc1c6e92c9a2b681c65ee753700a39bbef4b0d97
jp584 <jpv.9pm@gmail.com>
2024-03-09 00:37:17 -0400

    init state spring

---
+++ back_end_grado/src/main/java/grado/ucb/edu/back_end_grado/BackEndGradoApplication.java
@@ -0,0 +1,13 @@
+package grado.ucb.edu.back_end_grado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndGradoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndGradoApplication.class, args);
+	}
+
+}
---
+++ back_end_grado/src/test/java/grado/ucb/edu/back_end_grado/BackEndGradoApplicationTests.java
@@ -0,0 +1,13 @@
+package grado.ucb.edu.back_end_grado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndGradoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
