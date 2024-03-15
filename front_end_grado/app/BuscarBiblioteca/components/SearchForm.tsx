3a5bf27d727be43bda2070aa1d1599e37a5637b1
jp584 <jpv.9pm@gmail.com>
2024-03-14 23:24:30 -0400

    provider search form

--- front_end_grado/app/BuscarBiblioteca/components/SearchForm.tsx
+++ front_end_grado/app/BuscarBiblioteca/components/SearchForm.tsx
@@ -1,24 +1,43 @@
-import {Input} from "@nextui-org/react";
-import {Button} from "@nextui-org/react";
+import { Input } from "@nextui-org/react";
+import { Button } from "@nextui-org/react";
 import { FaPlus, FaMinus } from 'react-icons/fa';
 import React from 'react';
 import { useSearchForm } from "../providers/SearchFormProvider";
 
 const SearchForm = () => {
-    const {searchArray, addSearch , removeSearch} = useSearchForm();
-    return(
+    const { searchArray, addSearch, removeSearch } = useSearchForm();
+    return (
         <div className="flex items-center gap-4">
             <div>
-            <button onClick={() => addSearch('Example Search')}>Add Search</button>
-            {searchArray.map((search, index) => (
-                <div key={index}>
-                    {search}
-                    <button onClick={() => removeSearch()}>Remove</button>
-                </div>
-            ))}
-        </div>
-        
-        {/* <Input
+                <Input
+                    key="outside-left"
+                    type="email"
+                    label="Busqueda"
+                    labelPlacement="outside-left"
+                    description="Ingresa palabras claves"
+                />
+                <Button color="primary" variant="ghost" isIconOnly aria-label="Increase" onClick={() => addSearch('Example Search')}><FaPlus /></Button>
+                {searchArray.map((search, index) => (
+
+
+                    <div key={index}>
+                        <Input
+                            key="outside-left"
+                            type="email"
+                            label="Busqueda"
+                            labelPlacement="outside-left"
+                            description="Ingresa palabras claves"
+                        />
+                        <Button color="danger" variant="ghost" isIconOnly aria-label="Decrease" onClick={() => removeSearch()}>
+                            <FaMinus />
+                        </Button>
+
+
+                    </div>
+                ))}
+            </div>
+
+            {/* <Input
           key="outside-left"
           type="email"
           label="Busqueda"
@@ -31,7 +50,7 @@ const SearchForm = () => {
         <Button color="primary" variant="ghost" isIconOnly aria-label="Decrease">
           <FaMinus />
         </Button> */}
-      </div>
+        </div>
     );
 }
 export default SearchForm;
