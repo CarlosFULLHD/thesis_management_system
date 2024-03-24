e257562c1eb04945f85d044bf98ed463d9e57cc9
Natalia <bgutisol2019@gmail.com>
2024-03-23 22:31:19 -0400

    Avances login

--- front_end_grado/app/auth/login/page.tsx
+++ front_end_grado/app/auth/login/page.tsx
@@ -1,65 +1,67 @@
 "use client";
-import Image from "next/image";
-import googleLogo from "@/public/google.png";
-import githubLogo from "@/public/github.png";
-import {
-  GoogleSignInButton,
-  GithubSignInButton,
-} from "@/components/authButtons";
-import { getServerSession } from "next-auth";
-import { authConfig } from "@/lib/auth";
-import { useRouter } from "next/navigation";
-import { CredentialsForm } from "@/components/credentialsForm";
-import { useEffect } from "react";
-
-import React from "react";
-import {Card, CardHeader, CardBody} from "@nextui-org/react";
+import React, { useEffect } from 'react';
+import { BASE_URL } from "@/config/globals";
+import { getServerSession } from 'next-auth';
+import { authConfig } from '@/lib/auth';
+import { GoogleSignInButton } from '@/components/authButtons';
+import { Card, CardHeader, CardBody } from '@nextui-org/react';
+import { useRouter } from 'next/router';
 
 export default function SignInPage() {
   const router = useRouter();
 
-  useEffect(() => {
-    async function checkSession() {
-      const session = await getServerSession(authConfig);
+  // Definir checkSession fuera de useEffect para que sea accesible en todo el componente
+  async function checkSession() {
+    const session = await getServerSession(authConfig);
+    console.log("Session: ", session);
 
-      console.log("Session: ", session);
-
-      if (session) {
-        router.push("/dashboardInformation");
-      }
+    if (session && session.user) {
+      const email = session.user.email ?? ''; // Usar un string vacío como valor predeterminado
+      const role = await getRole(email);
+      console.log("Rol obtenido:", role);
+      router.push("/dashboardInformation");
     }
+  }
 
+  useEffect(() => {
     checkSession();
-  }, [router]);
+  }, [router]); // Asegúrate de que las dependencias estén correctas
+
+  async function getRole(email: string) {
+    const url = `${BASE_URL}/auth/getRole`;
+
+    try {
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email }),
+      });
+
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status} ${response.statusText}`);
+      }
+
+      const roleName = await response.text();
+      return roleName;
+    } catch (error) {
+      console.error('Error al obtener el rol:', error);
+      return '';
+    }
+  }
 
   return (
-    
     <div className="w-full flex flex-col items-center min-h-screen py-0">
       <div className="flex flex-col items-center mt-10 p-10 shadow-md">
         <Card className="py-0">
           <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-            <img
-                alt="Card background"
-                className="object-cover rounded-xl"
-                src="/../../../public/logo-sis.png"
-                height={150}
-                width={150}
-              />
-              <p className="text-tiny uppercase font-bold">Bienvenido</p>
-            <small className="text-default-500">Si ya tienes una cuenta registrada, ingresa al sistema utilizando<br></br> tu cuenta académica de Google terminada en 'ucb.edu.bo'</small>
-            {/*<h4 className="font-bold text-large">Sign In</h4>*/}
+            {/* Imagen y texto */}
           </CardHeader>
           <CardBody className="overflow-visible py-2">
-            <GoogleSignInButton />
+            <GoogleSignInButton onSignInSuccess={checkSession} />
           </CardBody>
         </Card>
-        
-        {/*<GithubSignInButton />
-          <span className="text-2xl font-semibold text-white text-center mt-8">
-            Or
-          </span>
-          <CredentialsSignInButton /> 
-        <CredentialsForm />*/}
       </div>
     </div>
   );
--- front_end_grado/components/authButtons.tsx
+++ front_end_grado/components/authButtons.tsx
@@ -5,21 +5,33 @@ import googleLogo from "@/public/google.png";
 import githubLogo from "@/public/github.png";
 import { signIn } from "next-auth/react";
 
-export function GoogleSignInButton() {
-  const handleClick = () => {
-    signIn("google");
+interface GoogleSignInButtonProps {
+  onSignInSuccess: () => void; // Tipando la función como una que no recibe argumentos y no retorna nada
+}
+
+export function GoogleSignInButton({ onSignInSuccess }: GoogleSignInButtonProps) {
+  const handleClick = async () => {
+    // Intenta iniciar sesión con Google
+    const result = await signIn("google", { redirect: false });
+
+    // Si el inicio de sesión es exitoso y se provee una función de éxito, llámala
+    if (result?.ok && onSignInSuccess) {
+      onSignInSuccess();
+    }
   };
 
   return (
     <button
       onClick={handleClick}
-      className="w-full flex items-center font-semibold justify-center h-14 px-6 mt-4 text-xl  transition-colors duration-300 bg-white border-2 border-black text-black rounded-lg focus:shadow-outline hover:bg-slate-200"
+      className="w-full flex items-center font-semibold justify-center h-14 px-6 mt-4 text-xl transition-colors duration-300 bg-white border-2 border-black text-black rounded-lg focus:shadow-outline hover:bg-slate-200"
     >
       <Image src={googleLogo} alt="Google Logo" width={20} height={20} />
       <span className="ml-4">Continue with Google</span>
     </button>
   );
 }
+
+
 
 export function GithubSignInButton() {
   const handleClick = () => {
