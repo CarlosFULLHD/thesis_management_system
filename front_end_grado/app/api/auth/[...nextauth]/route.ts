46d56f564ab796a9eddba988ccdfd6bd5ad86cfa
Natalia <bgutisol2019@gmail.com>
2024-03-25 09:38:37 -0400

    Problema roles, route, session token solucionado

--- front_end_grado/app/api/auth/[...nextauth]/route.ts
+++ front_end_grado/app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,11 @@
+import { BASE_URL } from "@/config/globals";
+import { useQuery } from "@tanstack/react-query"; // React query useQuery
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 
 async function fetchUserRole(email: string) {
     console.log(`Fetching role for email: ${email}`);
-    const url = `${process.env.BASE_URL}auth/getRole`;
+    const url = `${BASE_URL}auth/getRole`;
     const response = await fetch(url, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
@@ -25,25 +27,42 @@ const handler = NextAuth({
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID as string,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-        }),
+            authorization: {
+                params: {
+                    scope: 'openid profile email',
+                },
+            },
+        }),        
     ],
     callbacks: {
-        async session({ session, user }) {
+        async signIn({ user, account, profile, email, credentials }) {
+            console.log('---------------LLAMANDO CALLBACK----------------');
+            console.log('User:', user);
+            console.log('Credentials:', credentials);
+            console.log('Account:', account);
+            console.log('Profile:', profile);
+            console.log('Email:', email);
+            return true;
+        },
+        async session({ session, token }) {
             console.log('Session callback called');
-            const email = user?.email ?? '';
-            console.log(`Email from user: ${email}`);
-            if (email) {
+            console.log('Session object:', session);
+            console.log('Token object:', token);
+            session.user = session.user || {};
+    
+            if (token?.email) {
+                console.log(`Email from token: ${token.email}`);
+                session.user.email = token.email; 
+    
                 try {
-                    const role = await fetchUserRole(email);
+                    const role = await fetchUserRole(token.email);
                     console.log(`Role fetched: ${role}`);
-                    if (session.user) {
-                        session.user.role = role;
-                    } else {
-                        console.log('User is undefined in session');
-                    }
+                    session.user.role = role; // Asigna el rol al usuario en la sesión
                 } catch (error) {
                     console.error('Error fetching role:', error);
                 }
+            } else {
+                console.log('Email is undefined in token');
             }
             return session;
         }
