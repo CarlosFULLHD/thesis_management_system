e8cc6223ad9000f03f9341bc8a75895638ec4f41
CarlosFULLHD <cninareynaga@gmail.com>
2024-04-05 20:10:17 -0400

    Fix: Forms for student with new endpoint and database 200 OK

--- front_end_grado/app/(public)/_components/formRegistration.tsx
+++ front_end_grado/app/(public)/_components/formRegistration.tsx
@@ -7,8 +7,8 @@ import { Button } from "@nextui-org/button";
 import { useState } from "react";
 import { useStudents } from "../form/providers/studentProvider";
 export default function FormRegistration() {
-  const { addStudent } = useStudents(); 
-  const [driveLinks, setDriveLinks] = useState([""]);
+  const { addStudent } = useStudents();
+  // const [driveLinks, setDriveLinks] = useState([""]);
   const [studentData, setStudentData] = useState({
     ci: "",
     name: "",
@@ -20,52 +20,49 @@ export default function FormRegistration() {
   });
 
   // Handle the input change for student data
-  const handleInputChange = (e: { target: { name: any; value: any; }; }) => {
+  const handleInputChange = (e: { target: { name: any; value: any } }) => {
     const { name, value } = e.target;
     setStudentData((prev) => ({ ...prev, [name]: value }));
   };
 
   // Function to add a new Drive link input
-  const addDriveLink = () => {
-    setDriveLinks([...driveLinks, ""]);
-  };
-  const removeDriveLink = (index: number) => {
-    if (driveLinks.length > 1) {
-      const newDriveLinks = driveLinks.filter((_, i) => i !== index);
-      setDriveLinks(newDriveLinks);
-    }
-  };
+  // const addDriveLink = () => {
+  //   setDriveLinks([...driveLinks, ""]);
+  // };
+  // const removeDriveLink = (index: number) => {
+  //   if (driveLinks.length > 1) {
+  //     const newDriveLinks = driveLinks.filter((_, i) => i !== index);
+  //     setDriveLinks(newDriveLinks);
+  //   }
+  // };
   // Handle the change of each Drive link input
-  const handleDriveLinkChange = (index: number, value: string) => {
-    const newDriveLinks = [...driveLinks];
-    newDriveLinks[index] = value;
-    setDriveLinks(newDriveLinks);
-  };
+  // const handleDriveLinkChange = (index: number, value: string) => {
+  //   const newDriveLinks = [...driveLinks];
+  //   newDriveLinks[index] = value;
+  //   setDriveLinks(newDriveLinks);
+  // };
 
   // Handle form submission
-  const handleSubmit = async (e: { preventDefault: () => void; }) => {
+  const handleSubmit = async (e: { preventDefault: () => void }) => {
     e.preventDefault();
-        // Construir el objeto de datos del estudiante con los enlaces de Drive.
-        const formattedData = {
-          ...studentData,
-          pdfDriveUrls: driveLinks.filter(link => link) // Asegura que no se incluyan cadenas vacías
-        };
+    // Construir el objeto de datos del estudiante con los enlaces de Drive.
+    const formattedData = {
+      ...studentData,
+      // pdfDriveUrls: driveLinks.filter((link) => link),
+    };
 
-        try {
-          // Llamar al contexto del estudiante para agregar un nuevo estudiante.
-          await addStudent(formattedData);
-          alert("Formulario enviado con éxito.");
-          // Aquí manejarías la respuesta exitosa, como limpiar el formulario.
-        } catch (error) {
-          // Aquí manejarías errores de la petición.
-        }
-      };
-    // Combine studentData with driveLinks and send the POST request
-    console.log(studentData);
-    console.log(driveLinks);
-    // Here you would send the data to the server using POST request
-  
-  
+    try {
+      // Llamar al contexto del estudiante para agregar un nuevo estudiante.
+      await addStudent(formattedData);
+      alert("Formulario enviado con éxito.");
+      // Aquí manejarías la respuesta exitosa, como limpiar el formulario.
+    } catch (error) {
+      // Aquí manejarías errores de la petición.
+    }
+  };
+  // Combine studentData with driveLinks and send the POST request
+  console.log(studentData);
+  // Here you would send the data to the server using POST request
 
   return (
     <div className="flex flex-col gap-4">
@@ -151,7 +148,8 @@ export default function FormRegistration() {
           value={studentData.cellPhone}
           onChange={handleInputChange}
         />
-        {driveLinks.map((link, index) => (
+
+        {/* {driveLinks.map((link, index) => (
           <div key={index} className="flex items-center gap-2">
           <Input
             key={index}
@@ -174,12 +172,10 @@ export default function FormRegistration() {
             <Button color="primary" onClick={addDriveLink}>
               Añadir Link
             </Button>
-          )}
-
+          )} */}
 
         <Button type="submit">Enviar</Button>
       </form>
     </div>
   );
 }
-
--- front_end_grado/app/(public)/form/providers/studentProvider.tsx
+++ front_end_grado/app/(public)/form/providers/studentProvider.tsx
@@ -1,5 +1,5 @@
-import React, { createContext, useState, useContext } from 'react';
-import axios from 'axios';
+import React, { createContext, useState, useContext } from "react";
+import axios from "axios";
 
 interface DriveUrl {
   url: string;
@@ -14,31 +14,36 @@ interface StudentItem {
   description: string;
   email: string;
   cellPhone: string;
-  pdfDriveUrls: string[]; 
 }
 
 interface StudentContextType {
   students: StudentItem[];
-  addStudent: (studentData: Omit<StudentItem, 'idPerson'>) => Promise<void>;
+  addStudent: (studentData: Omit<StudentItem, "idPerson">) => Promise<void>;
 }
 
 const StudentContext = createContext<StudentContextType | undefined>(undefined);
 
-export const StudentProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+export const StudentProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
   const [students, setStudents] = useState<StudentItem[]>([]);
 
-  const addStudent = async (studentData: Omit<StudentItem, 'idPerson'>) => {
+  const addStudent = async (studentData: Omit<StudentItem, "idPerson">) => {
     try {
-      const response = await axios.post('http://127.0.0.1:8080/api/v1/student/register', studentData, {
-        headers: {
-          'Content-Type': 'application/json'
+      const response = await axios.post(
+        "http://127.0.0.1:8080/api/v1/student/register",
+        studentData,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
         }
-      });
-      if (response.data.status === '200') {
-        setStudents(prevStudents => [...prevStudents, response.data.result]);
+      );
+      if (response.data.status === "200") {
+        setStudents((prevStudents) => [...prevStudents, response.data.result]);
       }
     } catch (error) {
-      console.error('Error registering new student:', error);
+      console.error("Error registering new student:", error);
       throw error;
     }
   };
@@ -53,7 +58,7 @@ export const StudentProvider: React.FC<{ children: React.ReactNode }> = ({ child
 export const useStudents = (): StudentContextType => {
   const context = useContext(StudentContext);
   if (!context) {
-    throw new Error('useStudents must be used within a StudentProvider');
+    throw new Error("useStudents must be used within a StudentProvider");
   }
   return context;
 };
