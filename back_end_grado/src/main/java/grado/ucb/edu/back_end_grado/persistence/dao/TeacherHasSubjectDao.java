5028059dbbb252efb48d3cdc13ce6db03c0e431b
Cristopher <cristopher.olivera@ucb.edu.bo>
2024-04-15 16:30:34 -0400

    Feat: Teacher has subject Dao

--- back_end_grado/src/main/java/grado/ucb/edu/back_end_grado/persistence/dao/TeacherHasSubjectDao.java
+++ back_end_grado/src/main/java/grado/ucb/edu/back_end_grado/persistence/dao/TeacherHasSubjectDao.java
@@ -1,4 +1,10 @@
 package grado.ucb.edu.back_end_grado.persistence.dao;
 
-public interface TeacherHasSubjectDao {
+import grado.ucb.edu.back_end_grado.persistence.entity.TeacherHasSubjectEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TeacherHasSubjectDao extends JpaRepository<TeacherHasSubjectEntity, Long> {
+    List<TeacherHasSubjectEntity> findTeacherHasSubjectEntitiesByRoleHaspersonIdRolePerAndStatus(Long idRolePer, int status);
 }
