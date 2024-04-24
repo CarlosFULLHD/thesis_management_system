034e2d5f3fc4aaf17a26a89fc8b5cdf2afce1585
jp584 <jpv.9pm@gmail.com>
2024-04-24 09:36:05 -0400

    API DOCS 101

--- back_end_grado/src/main/java/grado/ucb/edu/back_end_grado/api/AcademicPeriodApi.java
+++ back_end_grado/src/main/java/grado/ucb/edu/back_end_grado/api/AcademicPeriodApi.java
@@ -6,10 +6,15 @@ import grado.ucb.edu.back_end_grado.dto.UnsuccessfulResponse;
 import grado.ucb.edu.back_end_grado.dto.request.AcademicPeriodRequest;
 import grado.ucb.edu.back_end_grado.dto.request.TaskRequest;
 import grado.ucb.edu.back_end_grado.util.Globals;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import org.apache.coyote.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.RequestContextHolder;
@@ -17,8 +22,13 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 
 @RestController
 @RequestMapping(Globals.apiVersion+"academic-period")
+@Tag(
+        name ="API - Periodo académico",
+        description = "Endpoint que permite la vinculación de un semestre a un periodo académico dando inicio y fin a un semestre"
+)
 public class AcademicPeriodApi {
-    private final AcademicPeriodBl academicPeriodBl;
+
+    private AcademicPeriodBl academicPeriodBl;
     private static final Logger LOG = LoggerFactory.getLogger(AcademicPeriodApi.class);
 
     public AcademicPeriodApi(AcademicPeriodBl academicPeriodBl) {
@@ -26,6 +36,10 @@ public class AcademicPeriodApi {
     }
 
     // Create new academic period
+    @Operation(
+            summary = "Crear un nuevo periodo académico",
+            description = "Añade un periodo académico, si el usuario tiene el rol de 'COORDINADOR'"
+    )
     @PostMapping("/")
     public ResponseEntity<Object> postAcademicPeriod(@RequestBody AcademicPeriodRequest requestAcademic) {
         Object finalResponse = academicPeriodBl.newAcademicPeriod(requestAcademic);
@@ -53,6 +67,24 @@ public class AcademicPeriodApi {
             responseCode = Integer.parseInt(((SuccessfulResponse) finalResponse).getStatus());
         } else if (finalResponse instanceof UnsuccessfulResponse) {
             LOG.error("LOG: No existen periodos académicos para esta fecha - " + ((UnsuccessfulResponse) finalResponse).getPath());
+            HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+            String requestPath = request.getRequestURI();
+            ((UnsuccessfulResponse) finalResponse).setPath(requestPath);
+            responseCode = Integer.parseInt(((UnsuccessfulResponse) finalResponse).getStatus());
+        }
+        return ResponseEntity.status(responseCode).body(finalResponse);
+    }
+
+    // Get active academic period by its id
+    @GetMapping("")
+    public ResponseEntity<Object> getActiveAcademicPeriodByItsId(@RequestParam("idAcad") final Long idAcad){
+        Object finalResponse = academicPeriodBl.getAcademicPeriodByItsId(idAcad);
+        int responseCode = 0;
+        if (finalResponse instanceof SuccessfulResponse) {
+            LOG.info("LOG: Periodo académico eliminado exitosamente");
+            responseCode = Integer.parseInt(((SuccessfulResponse) finalResponse).getStatus());
+        } else if (finalResponse instanceof UnsuccessfulResponse) {
+            LOG.error("LOG: Error al eliminar periodo académico - " + ((UnsuccessfulResponse) finalResponse).getPath());
             HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
             String requestPath = request.getRequestURI();
             ((UnsuccessfulResponse) finalResponse).setPath(requestPath);
