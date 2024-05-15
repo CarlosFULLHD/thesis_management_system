f8266eb48cbfca128eed200302d3a197247abe03
Cristopher <cristopher.olivera@ucb.edu.bo>
2024-05-14 20:34:42 -0400

    Feat: Pagination, sort & filter in Desertions

--- back_end_grado/src/main/java/grado/ucb/edu/back_end_grado/persistence/dao/DesertionDao.java
+++ back_end_grado/src/main/java/grado/ucb/edu/back_end_grado/persistence/dao/DesertionDao.java
@@ -1,7 +1,11 @@
 package grado.ucb.edu.back_end_grado.persistence.dao;
 
 import grado.ucb.edu.back_end_grado.persistence.entity.DesertionEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Arrays;
 import java.util.List;
@@ -9,4 +13,12 @@ import java.util.Optional;
 
 public interface DesertionDao extends JpaRepository<DesertionEntity, Long>{
 
+    @Query("SELECT d FROM desertion d WHERE d.status = :status AND " +
+            "(:filter IS NULL OR " +
+            "d.usersIdUsers.personIdPerson.ci iLIKE %:filter% OR " +
+            "d.usersIdUsers.personIdPerson.name iLIKE %:filter% OR " +
+            "d.usersIdUsers.personIdPerson.fatherLastName iLIKE %:filter% OR " +
+            "d.usersIdUsers.personIdPerson.motherLastName iLIKE %:filter% OR " +
+            "d.usersIdUsers.personIdPerson.email iLIKE %:filter% ) ")
+    Page<DesertionEntity> findAllDesertionEntities(@Param("filter") String filter, @Param("status") int status, Pageable pageable);
 }
