ef86d8abff05a417120d0334c014e39b137e9bde
CarlosFULLHD4K <cninareynaga@gmail.com>
2024-05-15 18:57:16 -0400

    Fix: fetch request with map lack of keys

--- front_end_grado/app/Gestion-tareas/Tareas-taller-uno/components/CollectionTasksWorkShopOne.tsx
+++ front_end_grado/app/Gestion-tareas/Tareas-taller-uno/components/CollectionTasksWorkShopOne.tsx
@@ -2,119 +2,121 @@ import { BASE_URL } from "@/config/globals";
 import { TaskItem, useTask } from "../../providers/TaskProvider";
 import { useQuery } from "@tanstack/react-query";
 import {
-    Card, CardBody, CardFooter, CardHeader, CircularProgress, Table,
-    TableHeader,
-    TableColumn,
-    TableBody,
-    TableRow,
-    TableCell,
-    Tooltip,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  CircularProgress,
+  Table,
+  TableHeader,
+  TableColumn,
+  TableBody,
+  TableRow,
+  TableCell,
+  Tooltip,
 } from "@nextui-org/react";
 import DeleteTaskButton from "./DeleteTaskButton";
 import UpateTaskButton from "./UpdateTaskButton";
 import WorkShopTitle from "./WorkShopTitle";
 
 const CollectionTasksWorkShopOne = () => {
-    // Importing data and method from provider
-    const { taskList, fetchTaskList } = useTask();
+  // Importing data and method from provider
+  const { taskList, fetchTaskList } = useTask();
 
+  // Fetch data function
+  const fetchData = async () =>
+    fetch(`${BASE_URL}task/work-shop?isGradeoneortwo=1`).then((res) =>
+      res.json()
+    );
 
-
-    // Fetch data function
-    const fetchData = async () => fetch(`${BASE_URL}task/work-shop?isGradeoneortwo=1`).then((res) => res.json());
-
-    // Loading state
-    const loadTaskList = (responseData: any) => {
-        // const taskMapItems: Map<number, TaskItem> = (new Map());
-        var taskListItems: TaskItem[] = [];
-        if (responseData.status == 200) {
-            taskListItems = responseData["result"].map((task: TaskItem) => ({
-                idTask: task.idTask,
-                titleTask: task.titleTask,
-                task: task.task,
-                isGradeoneortwo: task.isGradeoneortwo,
-                status: task.status,
-                createdAt: task.createdAt // Convert string to Date object
-            }));
-
-        }
-        fetchTaskList(taskListItems); // Changing provider state
+  // Loading state
+  const loadTaskList = (responseData: any) => {
+    // const taskMapItems: Map<number, TaskItem> = (new Map());
+    var taskListItems: TaskItem[] = [];
+    if (responseData.status == 200) {
+      taskListItems = responseData["result"].map((task: TaskItem) => ({
+        idTask: task.idTask,
+        titleTask: task.titleTask,
+        task: task.task,
+        isGradeoneortwo: task.isGradeoneortwo,
+        status: task.status,
+        createdAt: task.createdAt, // Convert string to Date object
+      }));
     }
+    fetchTaskList(taskListItems); // Changing provider state
+  };
 
-    //Query that fetches the end point, being called as soon the component builds it self
-    const { isLoading, isError } = useQuery({
-        queryKey: ["taskTable"],
-        queryFn: async () => {
-            const data = await fetchData();
-            loadTaskList(data)
-            return data
-        }
-    })
-    // Fetching state
-    if (isLoading) {
-        return <CircularProgress aria-label="Cargando..." />;
-    }
-    // Error state
-    if (isError) {
-        return <div>Oops!</div>;
-    }
-    // Success state
-    if (taskList.length > 0) {
-        return (
-            <div>
-                <WorkShopTitle />
+  //Query that fetches the end point, being called as soon the component builds it self
+  const { isLoading, isError } = useQuery({
+    queryKey: ["taskTable"],
+    queryFn: async () => {
+      const data = await fetchData();
+      loadTaskList(data);
+      return data;
+    },
+  });
+  // Fetching state
+  if (isLoading) {
+    return <CircularProgress aria-label="Cargando..." />;
+  }
+  // Error state
+  if (isError) {
+    return <div>Oops!</div>;
+  }
+  // Success state
+  if (taskList.length > 0) {
+    return (
+      <div>
+        <WorkShopTitle />
 
-                {taskList.map((item, index) => (
-
-                    <Card className="m-8">
-                        <CardHeader>
-                            <div className="flex flex-col">
-                                <p><b>Titulo: </b> {item.titleTask}</p>
-                            </div>
-                        </CardHeader>
-                        <CardBody>
-                            <Table aria-label="Example dynamic collection table">
-                                <TableHeader>
-                                    <TableColumn>Fecha creación</TableColumn>
-                                    <TableColumn>Descripción</TableColumn>
-                                </TableHeader>
-                                <TableBody>
-                                    <TableRow key={item.idTask}>
-                                        <TableCell>{item.createdAt}</TableCell>
-                                        <TableCell>{item.task}</TableCell>
-                                    </TableRow>
-                                </TableBody>
-                            </Table>
-                        </CardBody>
-                        <CardFooter>
-                            <div className="flex items-center gap-8">
-                                <Tooltip color="primary" content="Editar tarea">
-                                    <span className="text-lg text-primary cursor-pointer active:opacity-50">
-                                        <UpateTaskButton idTask={item.idTask} />
-                                    </span>
-                                </Tooltip>
-                                <Tooltip color="danger" content="Borrar tarea">
-                                    <span className="text-lg text-danger cursor-pointer active:opacity-50">
-                                        <DeleteTaskButton idTask={item.idTask} />
-                                    </span>
-                                </Tooltip>
-                            </div>
-                        </CardFooter>
-                    </Card>
-
-                ))}
-
-
-
-            </div>
-        );
-    }
-    else {
-        return <div>
-            <h1>No existen tareas para taller de grado 1</h1></div>;
-    }
-}
-
-
+        {taskList.map((item, index) => (
+          <Card key={item.idTask} className="m-8">
+            <CardHeader>
+              <div className="flex flex-col">
+                <p>
+                  <b>Titulo: </b> {item.titleTask}
+                </p>
+              </div>
+            </CardHeader>
+            <CardBody>
+              <Table aria-label="Example dynamic collection table">
+                <TableHeader>
+                  <TableColumn>Fecha creación</TableColumn>
+                  <TableColumn>Descripción</TableColumn>
+                </TableHeader>
+                <TableBody>
+                  <TableRow>
+                    <TableCell>{item.createdAt}</TableCell>
+                    <TableCell>{item.task}</TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+            </CardBody>
+            <CardFooter>
+              <div className="flex items-center gap-8">
+                <Tooltip color="primary" content="Editar tarea">
+                  <span className="text-lg text-primary cursor-pointer active:opacity-50">
+                    <UpateTaskButton idTask={item.idTask} />
+                  </span>
+                </Tooltip>
+                <Tooltip color="danger" content="Borrar tarea">
+                  <span className="text-lg text-danger cursor-pointer active:opacity-50">
+                    <DeleteTaskButton idTask={item.idTask} />
+                  </span>
+                </Tooltip>
+              </div>
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h1>No existen tareas para taller de grado 1</h1>
+      </div>
+    );
+  }
+};
 
 export default CollectionTasksWorkShopOne;
--- front_end_grado/app/Hito-coordinador/Listar-periodo/components/MilestoneCollection.tsx
+++ front_end_grado/app/Hito-coordinador/Listar-periodo/components/MilestoneCollection.tsx
@@ -1,190 +1,262 @@
 import { useQuery } from "@tanstack/react-query";
 import { useMilestoneCollection } from "../providers/MilestoneCollectionProvider";
-import { CircularProgress, Card, CardHeader, Divider, CardBody, CardFooter, Avatar, Button, Link, Chip } from "@nextui-org/react";
-import { useRouter, useSearchParams, usePathname, useParams } from "next/navigation";
-import { FaEnvelope } from 'react-icons/fa';
-
+import {
+  CircularProgress,
+  Card,
+  CardHeader,
+  Divider,
+  CardBody,
+  CardFooter,
+  Avatar,
+  Button,
+  Link,
+  Chip,
+} from "@nextui-org/react";
+import {
+  useRouter,
+  useSearchParams,
+  usePathname,
+  useParams,
+} from "next/navigation";
+import { FaEnvelope } from "react-icons/fa";
 
 const MilestoneCollection = () => {
-    // Importing data and methods from provider
-    const { milestoneList, loadMilestonesByAcademicPeriod } = useMilestoneCollection();
-    // Routing instance and params
-    const { replace } = useRouter();
-    const searchParams = useSearchParams();
+  // Importing data and methods from provider
+  const { milestoneList, loadMilestonesByAcademicPeriod } =
+    useMilestoneCollection();
+  // Routing instance and params
+  const { replace } = useRouter();
+  const searchParams = useSearchParams();
 
-    // Map for colors
-    const colorsMap: Map<number, string> = new Map([
-        [1, "bg-custom-purple"],  // EN ESPERA
-        [2, "bg-danger"], // DESAPROBADO
-        [3, "bg-custom-yellow"],// OBSERVADO
-        [4, "bg-success"], // APROBADO
-        [5, "bg-custom-blue"], // ABIERTO
-        [6, "bg-danger"],// CERRADO
-        [7, "bg-danger"],// SIN PRESENTAR
-        [8, "bg-danger"]// PRESENTO TARDE
-    ]);
+  // Map for colors
+  const colorsMap: Map<number, string> = new Map([
+    [1, "bg-custom-purple"], // EN ESPERA
+    [2, "bg-danger"], // DESAPROBADO
+    [3, "bg-custom-yellow"], // OBSERVADO
+    [4, "bg-success"], // APROBADO
+    [5, "bg-custom-blue"], // ABIERTO
+    [6, "bg-danger"], // CERRADO
+    [7, "bg-danger"], // SIN PRESENTAR
+    [8, "bg-danger"], // PRESENTO TARDE
+  ]);
 
-    // Function to add params into the url and redirect to the review form
-    const addParamsToUrl = (idMilestone: string, userId: string) => {
-        const params = new URLSearchParams(searchParams);
-        if (idMilestone && userId) {
-            params.set('idMilestone', idMilestone);
-            params.set('userId', userId);
-        } else {
-            params.delete('idMilestone');
-            params.delete('userId')
-        }
-        replace(`/Hito-coordinador/Revisar-estudiante?${params.toString()}`)
+  // Function to add params into the url and redirect to the review form
+  const addParamsToUrl = (idMilestone: string, userId: string) => {
+    const params = new URLSearchParams(searchParams);
+    if (idMilestone && userId) {
+      params.set("idMilestone", idMilestone);
+      params.set("userId", userId);
+    } else {
+      params.delete("idMilestone");
+      params.delete("userId");
     }
+    replace(`/Hito-coordinador/Revisar-estudiante?${params.toString()}`);
+  };
 
-    // Function to add params into the url and redirect to the details panel
-    const addParamsToDetailsUrl = (idMilestone: string, userId: string) => {
-        const params = new URLSearchParams(searchParams);
-        if (idMilestone && userId) {
-            params.set('idMilestone', idMilestone);
-            params.set('userId', userId);
-        } else {
-            params.delete('idMilestone');
-            params.delete('userId')
-        }
-        replace(`/Hito-coordinador/Detalle-estudiante?${params.toString()}`)
+  // Function to add params into the url and redirect to the details panel
+  const addParamsToDetailsUrl = (idMilestone: string, userId: string) => {
+    const params = new URLSearchParams(searchParams);
+    if (idMilestone && userId) {
+      params.set("idMilestone", idMilestone);
+      params.set("userId", userId);
+    } else {
+      params.delete("idMilestone");
+      params.delete("userId");
     }
+    replace(`/Hito-coordinador/Detalle-estudiante?${params.toString()}`);
+  };
 
-    // Routing method to the review form
-    const goReviewStudent = (idMilestone: number, idUsers: number) => {
-        return () => {
-            addParamsToUrl(idMilestone.toString(), idUsers.toString())
-        };
-    }
+  // Routing method to the review form
+  const goReviewStudent = (idMilestone: number, idUsers: number) => {
+    return () => {
+      addParamsToUrl(idMilestone.toString(), idUsers.toString());
+    };
+  };
 
-    // Routing method to the details panel
-    const goDetailsStudent = (idMilestone: number, idUsers: number) => {
-        return () => {
-            addParamsToDetailsUrl(idMilestone.toString(), idUsers.toString())
-        }
-    }
+  // Routing method to the details panel
+  const goDetailsStudent = (idMilestone: number, idUsers: number) => {
+    return () => {
+      addParamsToDetailsUrl(idMilestone.toString(), idUsers.toString());
+    };
+  };
 
+  const { isLoading, isError } = useQuery({
+    queryKey: ["milestont"],
+    queryFn: async () => {
+      await loadMilestonesByAcademicPeriod();
+      return milestoneList;
+    },
+  });
+  // Fetching state
+  if (isLoading) {
+    return <CircularProgress aria-label="Cargando..." />;
+  }
+  // Error state
+  if (isError) {
+    return <div>Oops!</div>;
+  }
+  // Success state
+  if (milestoneList.length > 0) {
+    return (
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {milestoneList.map((item) => (
+          <Card key={item.idMilestone} className="m-8">
+            <CardHeader className="justify-between">
+              <div className="flex gap-5">
+                <Avatar
+                  className="bg-blue-500 font-bold"
+                  isBordered
+                  radius="full"
+                  size="md"
+                  name={`${item.usersIdUsers.personIdPerson.name.charAt(0).toUpperCase()}${item.usersIdUsers.personIdPerson.fatherLastName.charAt(0).toUpperCase()}${item.usersIdUsers.personIdPerson.motherLastName.charAt(0).toUpperCase()}`}
+                />
+                <div className="flex flex-col gap-1 items-start justify-center">
+                  <h4 className="text-small font-semibold leading-none text-default-600">{`${item.usersIdUsers.personIdPerson.name} ${item.usersIdUsers.personIdPerson.fatherLastName}`}</h4>
+                </div>
+              </div>
 
-    const { isLoading, isError } = useQuery({
-        queryKey: ["milestont"],
-        queryFn: async () => {
-            await loadMilestonesByAcademicPeriod();
-            return milestoneList;
-        }
-    })
-    // Fetching state
-    if (isLoading) {
-        return <CircularProgress aria-label="Cargando..." />;
-    }
-    // Error state
-    if (isError) {
-        return <div>Oops!</div>;
-    }
-    // Success state
-    if (milestoneList.length > 0) {
-        return (
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                {
-                    milestoneList.map((item) => (
-                        <Card className="m-8"
-                        >
-                            <CardHeader className="justify-between">
-                                <div className="flex gap-5">
-                                    <Avatar
-                                        className="bg-blue-500 font-bold"
-                                        isBordered
-                                        radius="full"
-                                        size="md"
-                                        name={`${item.usersIdUsers.personIdPerson.name.charAt(0).toUpperCase()}${item.usersIdUsers.personIdPerson.fatherLastName.charAt(0).toUpperCase()}${item.usersIdUsers.personIdPerson.motherLastName.charAt(0).toUpperCase()}`}
-                                    />
-                                    <div className="flex flex-col gap-1 items-start justify-center">
-                                        <h4 className="text-small font-semibold leading-none text-default-600">{`${item.usersIdUsers.personIdPerson.name} ${item.usersIdUsers.personIdPerson.fatherLastName}`}</h4>
-                                    </div>
-                                </div>
+              {/* REVIEW BUTTON => (EN ESPERA)*/}
+              {item.taskStatesIdTaskState.idTaskState == 1 ? (
+                <Button
+                  className={colorsMap.get(
+                    item.taskStatesIdTaskState.idTaskState
+                  )}
+                  radius="full"
+                  size="sm"
+                  variant="flat"
+                  onPress={goReviewStudent(
+                    item.idMilestone,
+                    item.usersIdUsers.idUsers
+                  )}
+                >
+                  Revisar
+                  {/* DETAILS BUTTON => (DESAPROBADO, OBSERVADO,APROBADO) */}
+                </Button>
+              ) : item.taskStatesIdTaskState.idTaskState == 2 ||
+                item.taskStatesIdTaskState.idTaskState == 3 ||
+                item.taskStatesIdTaskState.idTaskState == 4 ? (
+                <Button
+                  className={colorsMap.get(
+                    item.taskStatesIdTaskState.idTaskState
+                  )}
+                  radius="full"
+                  size="sm"
+                  variant="flat"
+                  onPress={goDetailsStudent(
+                    item.idMilestone,
+                    item.usersIdUsers.idUsers
+                  )}
+                >
+                  Detalles
+                </Button>
+              ) : (
+                <></>
+              )}
+            </CardHeader>
+            <Divider />
 
-                                {/* REVIEW BUTTON => (EN ESPERA)*/}
-                                {item.taskStatesIdTaskState.idTaskState == 1
-                                    ? <Button
-                                        className={colorsMap.get(item.taskStatesIdTaskState.idTaskState)}
-                                        radius="full"
-                                        size="sm"
-                                        variant="flat"
-                                        onPress={(goReviewStudent(item.idMilestone, item.usersIdUsers.idUsers))}
+            <CardBody>
+              {/* In case the student has been OBSERVADO or the letter has recently created ABIERTO */}
+              {item.taskStatesIdTaskState.idTaskState == 5 ? (
+                <p className="text-s font-bold italic uppercase tracking-wide text-center">
+                  <Chip
+                    className={colorsMap.get(
+                      item.taskStatesIdTaskState.idTaskState
+                    )}
+                    variant="faded"
+                  >
+                    Esperando acción estudiante
+                  </Chip>
+                </p>
+              ) : // In case there are url or comments
+              item.taskStatesIdTaskState.idTaskState == 3 ? (
+                <>
+                  <p className="text-s font-bold italic uppercase tracking-wide text-center">
+                    <Chip
+                      className={colorsMap.get(
+                        item.taskStatesIdTaskState.idTaskState
+                      )}
+                      variant="faded"
+                    >
+                      Esperando acción estudiante
+                    </Chip>
+                  </p>{" "}
+                  <div>
+                    {item.comments != "" ? (
+                      <div>
+                        <p>
+                          <b>Observaciones:</b>
+                        </p>{" "}
+                        <p>{item.comments}</p>{" "}
+                      </div>
+                    ) : (
+                      <></>
+                    )}
+                    <div className="flex justify-center mt-4">
+                      <Chip
+                        className={colorsMap.get(
+                          item.taskStatesIdTaskState.idTaskState
+                        )}
+                      >
+                        <Link href={item.url} target="_blank">
+                          <FaEnvelope />
+                          Carta postulación
+                          <FaEnvelope />
+                        </Link>
+                      </Chip>
+                    </div>
+                  </div>
+                </>
+              ) : (
+                <div>
+                  {item.comments != "" ? (
+                    <div>
+                      <p>
+                        <b>Observaciones:</b>
+                      </p>{" "}
+                      <p>{item.comments}</p>{" "}
+                    </div>
+                  ) : (
+                    <></>
+                  )}
+                  <div className="flex justify-center mt-4">
+                    <Chip
+                      className={colorsMap.get(
+                        item.taskStatesIdTaskState.idTaskState
+                      )}
+                    >
+                      <Link href={item.url} target="_blank">
+                        <FaEnvelope />
+                        Carta postulación
+                        <FaEnvelope />
+                      </Link>
+                    </Chip>
+                  </div>
+                </div>
+              )}
+            </CardBody>
 
-                                    >
-                                        Revisar
-                                        {/* DETAILS BUTTON => (DESAPROBADO, OBSERVADO,APROBADO) */}
-                                    </Button>
-                                    : item.taskStatesIdTaskState.idTaskState == 2 || item.taskStatesIdTaskState.idTaskState == 3 || item.taskStatesIdTaskState.idTaskState == 4 ?
-                                        <Button
-                                            className={colorsMap.get(item.taskStatesIdTaskState.idTaskState)}
-                                            radius="full"
-                                            size="sm"
-                                            variant="flat"
-                                            onPress={(goDetailsStudent(item.idMilestone, item.usersIdUsers.idUsers))}
-
-                                        >
-                                            Detalles
-                                        </Button>
-                                        : <></>
-                                }
-                            </CardHeader>
-                            <Divider />
-
-                            <CardBody>
-                                {/* In case the student has been OBSERVADO or the letter has recently created ABIERTO */}
-                                {item.taskStatesIdTaskState.idTaskState == 5
-                                    ?
-                                    <p className="text-s font-bold italic uppercase tracking-wide text-center">
-                                        <Chip className={colorsMap.get(item.taskStatesIdTaskState.idTaskState)} variant="faded">Esperando acción estudiante</Chip>
-                                    </p>
-                                    // In case there are url or comments
-                                    : item.taskStatesIdTaskState.idTaskState == 3
-                                        ? <>
-                                            <p className="text-s font-bold italic uppercase tracking-wide text-center">
-                                                <Chip className={colorsMap.get(item.taskStatesIdTaskState.idTaskState)} variant="faded">Esperando acción estudiante</Chip>
-                                            </p> <div>
-                                                {item.comments != ""
-                                                    ? <div><p><b>Observaciones:</b></p> <p>{item.comments}</p> </div>
-                                                    : <></>}
-                                                <div className="flex justify-center mt-4">
-                                                    <Chip className={colorsMap.get(item.taskStatesIdTaskState.idTaskState)}><Link href={item.url} target="_blank"><FaEnvelope />Carta postulación<FaEnvelope /></Link></Chip>
-                                                </div>
-                                            </div>
-                                        </>
-
-                                        : <div>
-                                            {item.comments != ""
-                                                ? <div><p><b>Observaciones:</b></p> <p>{item.comments}</p> </div>
-                                                : <></>}
-                                            <div className="flex justify-center mt-4">
-                                                <Chip className={colorsMap.get(item.taskStatesIdTaskState.idTaskState)}><Link href={item.url} target="_blank"><FaEnvelope />Carta postulación<FaEnvelope /></Link></Chip>
-                                            </div>
-                                        </div>
-                                }
-                            </CardBody>
-
-                            <Divider />
-                            <CardFooter className={`flex justify-center items-center ${colorsMap.get(item.taskStatesIdTaskState.idTaskState)}`}>
-                                <div className="text-center rounded ">
-                                    <p className="text-xs uppercase tracking-wide">Estado</p>
-                                    <p className="font-bold text-xl">
-                                        {/* Message to be show when the student sends its letter for the first time or corrects an observation */}
-                                        {item.taskStatesIdTaskState.idTaskState == 1
-                                            ? "LISTA PARA REVISIÓN"
-                                            : item.taskStatesIdTaskState.idTaskState == 5
-                                                ? "EN ESPERA DE CARTA"
-                                                : item.taskStatesIdTaskState.description
-                                        }
-                                    </p>
-                                </div>
-                            </CardFooter>
-                        </Card>
-                    ))
-                }
-            </div >
-        );
-    }
-}
+            <Divider />
+            <CardFooter
+              className={`flex justify-center items-center ${colorsMap.get(item.taskStatesIdTaskState.idTaskState)}`}
+            >
+              <div className="text-center rounded ">
+                <p className="text-xs uppercase tracking-wide">Estado</p>
+                <p className="font-bold text-xl">
+                  {/* Message to be show when the student sends its letter for the first time or corrects an observation */}
+                  {item.taskStatesIdTaskState.idTaskState == 1
+                    ? "LISTA PARA REVISIÓN"
+                    : item.taskStatesIdTaskState.idTaskState == 5
+                      ? "EN ESPERA DE CARTA"
+                      : item.taskStatesIdTaskState.description}
+                </p>
+              </div>
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
+    );
+  }
+};
 
 export default MilestoneCollection;
