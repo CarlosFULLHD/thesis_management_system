403af8a1ee8a5308793d99ba025b99983419cd44
CarlosFULLHD <cninareynaga@gmail.com>
2024-05-16 18:14:07 -0400

    Fix: image optimization off because of Netlify

--- front_end_grado/next.config.js
+++ front_end_grado/next.config.js
@@ -3,6 +3,9 @@
 const nextConfig = {
   reactStrictMode: true,
   output: "export", // Añadido para habilitar la exportación estática
+  images: {
+    unoptimized: true,
+  },
   // Configuraciones opcionales, descomentar si es necesario:
   // trailingSlash: true,
   // distDir: 'dist',
