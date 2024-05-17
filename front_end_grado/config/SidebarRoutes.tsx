92e320cd3eb20b63d01fdd396089a0865d6b4c63
CarlosFULLHD <cninareynaga@gmail.com>
2024-05-17 18:49:48 -0400

    Feat: adding test

--- front_end_grado/config/SidebarRoutes.tsx
+++ front_end_grado/config/SidebarRoutes.tsx
@@ -43,7 +43,7 @@ const routesConfig: RoutesConfig = {
     { icon: ClipboardPen, label: "Inscribirme", href: "/form" },
     {
       icon: Search,
-      label: "Herramienta de ayuda: Proyectos Pasados",
+      label: "Herramienta de ayuda: Proyectos de anteriores semestres",
       href: "/Buscar-biblioteca",
     },
     {
