ee7a2fa0637ed2e2c9dc67896d28aa146aec68fe
CarlosFULLHD <cninareynaga@gmail.com>
2024-05-22 23:09:24 -0400

    Feat: Patch social network url linkedin by userid and socialnetwork id

--- back_end_grado/src/main/java/grado/ucb/edu/back_end_grado/dto/request/SocialNetworkUpdateRequest.java
+++ back_end_grado/src/main/java/grado/ucb/edu/back_end_grado/dto/request/SocialNetworkUpdateRequest.java
@@ -1,4 +1,16 @@
 package grado.ucb.edu.back_end_grado.dto.request;
 
 public class SocialNetworkUpdateRequest {
+    private String urlLinkedin;
+
+
+    // Getters and Setters
+    public String getUrlLinkedin() {
+        return urlLinkedin;
+    }
+
+    public void setUrlLinkedin(String urlLinkedin) {
+        this.urlLinkedin = urlLinkedin;
+    }
+
 }
