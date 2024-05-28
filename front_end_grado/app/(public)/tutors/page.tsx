4bfa34473ba75441dbbe3dc443771e658c7a6d21
CarlosFULLHD <cninareynaga@gmail.com>
2024-05-27 23:04:03 -0400

    Title tutors

--- front_end_grado/app/(public)/tutors/page.tsx
+++ front_end_grado/app/(public)/tutors/page.tsx
@@ -8,6 +8,9 @@ const App = () => {
   return (
     <ReactQueryClientProvider>
       <TutorsProvider>
+        <h1 className="text-center text-3xl md:text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-light to-blue-dark dark:from-yellow-light dark:to-yellow-dark pt-6">
+          Tutores U.C.B.
+        </h1>
         <TutorsList />
       </TutorsProvider>
     </ReactQueryClientProvider>
