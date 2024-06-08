79ee1abc2850a2a87ab28a1d31d764b23580626a
Cristopher <cristopher.olivera@ucb.edu.bo>
2024-06-07 20:44:38 -0400

    Fix: Pagination in PublicInformation

--- back_end_grado/src/main/java/grado/ucb/edu/back_end_grado/persistence/dao/PublicInformationDao.java
+++ back_end_grado/src/main/java/grado/ucb/edu/back_end_grado/persistence/dao/PublicInformationDao.java
@@ -2,6 +2,7 @@ package grado.ucb.edu.back_end_grado.persistence.dao;
 
 import grado.ucb.edu.back_end_grado.persistence.entity.PublicInformationEntity;
 import jakarta.transaction.Transactional;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -30,6 +31,6 @@ public interface PublicInformationDao extends JpaRepository<PublicInformationEnt
     int patchEntry(@Param("idUsers") Long idUsers, @Param("title") String title, @Param("information") String information, @Param("idPublicInfo") Long idPublicInfo, @Param("publicationDate") LocalDateTime publicationDate, @Param("deadline") LocalDateTime deadline);
 
     @Query("SELECT p FROM public_information p WHERE p.status = 1 AND p.publicationDate <= CURRENT_TIMESTAMP AND p.deadline >= CURRENT_TIMESTAMP")
-    List<PublicInformationEntity> findActivePublicInformationWithinCurrentTime(Pageable pageable);
+    Page<PublicInformationEntity> findActivePublicInformationWithinCurrentTime(Pageable pageable);
 
 }
