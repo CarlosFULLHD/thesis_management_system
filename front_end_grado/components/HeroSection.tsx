7ed5560ffbbe22b6e94cdefb154a0084797583ab
Nat Ubuntu <bgutisol2019@gmail.com>
2024-06-20 13:21:42 -0400

    home actualizado y ordenado

--- front_end_grado/components/HeroSection.tsx
+++ front_end_grado/components/HeroSection.tsx
@@ -1,9 +1,25 @@
 import React from "react";
-import { Accordion, AccordionItem, Avatar, Button } from "@nextui-org/react";
+import { Accordion, AccordionItem, Avatar, Button, Card, CardBody, CardHeader } from "@nextui-org/react";
+import { GraduationCap,
+  LaptopIcon,
+  BriefcaseBusiness,
+  ClipboardList,
+  MailPlus,
+  CalendarClock
+ } from "lucide-react";
 
 export const HeroSection = () => {
   const defaultContent =
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+
+  const scrollToSection = (id: string) => {
+    const section = document.getElementById(id);
+    if (section) {
+      const yOffset = -100; // Ajusta este valor según sea necesario
+      const y = section.getBoundingClientRect().top + window.pageYOffset + yOffset;
+      window.scrollTo({ top: y, behavior: "smooth" });
+    }
+  };
 
   return (
     <>
@@ -17,29 +33,40 @@ export const HeroSection = () => {
                 Sistema de Talleres de Grado
               </span>
             </h1>
-            <h2 className="text-mg tracking-tighter pt-3 md:px-32 lg:text-2xl xl:text-2xl">
+            <h2 className="text-lg tracking-tighter pt-3 md:px-32 lg:text-2xl xl:text-2xl">
               Nuestro sistema te acompaña en cada paso para alcanzar tus metas
               académicas de manera efectiva y organizada.
             </h2>
 
             <div className="flex flex-col items-center lg:flex-row lg:justify-center lg:items-center lg:gap-10 lg:w-full">
-              <div className="w-full px-5 lg:w-auto">
+              <div className="w-full px-2 lg:w-auto">
                 <Button
-                  onClick={() => {
-                    const section = document.getElementById("modalidades");
-                    if (section) {
-                      section.scrollIntoView({ behavior: "smooth" });
-                    }
-                  }}
-                  className="w-full md:w-80 lg:mt-7 mx-auto text-xl font-bold bg-yellow-light dark:bg-yellow-dark hover:text-black shadow-lg shadow-blue-light  text-white py-6 px-6 lg:px-10 flex items-center justify-center relative transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500"
+                  onClick={() => scrollToSection("modalidades")}
+                  className="w-full md:w-50 lg:mt-7 mx-auto text-xl font-bold bg-yellow-light dark:bg-yellow-dark hover:text-black shadow-lg shadow-blue-light text-white py-6 px-4 lg:px-5 flex items-center justify-center relative transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500"
                 >
-                  Descubre Más
+                  Modalidades
+                </Button>
+              </div>
+              <div className="w-full px-2 lg:w-auto">
+                <Button
+                  onClick={() => scrollToSection("instrucciones")}
+                  className="w-full md:w-50 lg:mt-7 mx-auto text-xl font-bold bg-yellow-light dark:bg-yellow-dark hover:text-black shadow-lg shadow-blue-light text-white py-6 px-4 lg:px-5 flex items-center justify-center relative transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500"
+                >
+                  Instrucciones
+                </Button>
+              </div>
+              <div className="w-full px-2 lg:w-auto">
+                <Button
+                  onClick={() => scrollToSection("noticias")}
+                  className="w-full md:w-50 lg:mt-7 mx-auto text-xl font-bold bg-yellow-light dark:bg-yellow-dark hover:text-black shadow-lg shadow-blue-light text-white py-6 px-4 lg:px-5 flex items-center justify-center relative transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500"
+                >
+                  Noticias
                 </Button>
               </div>
               <div className="w-full px-4 lg:w-auto mt-4 lg:mt-0 lg:px-0">
                 <Button
                   href="#inscripcion"
-                  className="w-full md:w-80 mx-auto text-xl lg:text-xl font-bold text-yellow-dark md:mt-6  py-6 px-6 lg:px-10 flex items-center justify-center"
+                  className="w-full md:w-50 mx-auto text-xl lg:text-xl font-bold text-yellow-dark md:mt-6 py-6 px-4 lg:px-5 flex items-center justify-center"
                 >
                   Inscríbete Ahora ⮕
                 </Button>
@@ -47,86 +74,75 @@ export const HeroSection = () => {
             </div>
           </div>
 
-          <div
-            id="modalidades"
-            className="relative py-6 md:mt-16 px-4 lg:px-16 mx-4 w-full"
-          >
-            <div className="flex flex-wrap">
-              <div className="w-full lg:w-1/3 px-4">
+          <div id="modalidades" className="relative py-6 md:mt-16 px-4 lg:px-16 mx-4 w-full">
+            <h2 className="text-3xl font-bold tracking-tighter lg:text-2xl xl:text-5xl md:px-16 mb-8">
+              Modalidades de Graduación
+            </h2>
+            <p className="text-xl lg:text-xl mb-4">
+              Existen dos modalidades principales para tu graduación:
+            </p>
+            <div className="flex flex-wrap justify-center">
+              <div className="w-full lg:w-1/3 px-4 mb-4">
+                <Card>
+                  <CardHeader>
+                  <div className={`border-2 p-2 rounded-lg border-warning`}>
+                    <LaptopIcon size={24} />
+                  </div>
+                    <h4 className="ml-4">Tesis/Proyecto Grado</h4>
+                  </CardHeader>
+                  <CardBody>
+                    <h5 className="text-sm">
+                      Necesitas proponer un tema. Revisa proyectos anteriores en la pestaña "Herramienta de Proyectos Pasados".
+                    </h5>
+                  </CardBody>
+                </Card>
+              </div>
+              <div className="w-full lg:w-1/3 px-4 mb-4">
+                <Card>
+                  <CardHeader>
+                  <div className={`border-2 p-2 rounded-lg border-primary-500`}>
+                    <GraduationCap size={24} />
+                  </div>
+                    <h4 className="ml-4">Graduación Excelencia</h4>
+                  </CardHeader>
+                  <CardBody>
+                    <h5 className="text-sm">
+                      Necesitas tener notas por encima de 90, sin ninguna materia reprobada o abandonada en toda la malla curricular.
+                    </h5>
+                  </CardBody>
+                </Card>
+              </div>
+              <div className="w-full lg:w-1/3 px-4 mb-4">
+                <Card>
+                  <CardHeader>
+                  <div className={`border-2 p-2 rounded-lg border-warning`}>
+                    <BriefcaseBusiness size={24} />
+                  </div>
+                    <h4 className="ml-4">Trabajo Dirigido</h4>
+                  </CardHeader>
+                  <CardBody>
+                    <h5 className="text-sm">
+                      Necesitarás hacer un trabajo específico en una empresa, esto se coordina junto a un tutor de la empresa a la que harás el trabajo, al igual que un tutor para la UCB que vea tu progreso. Tienes que cumplir un objetivo dentro de la empresa para aprobar exitosamente.
+                    </h5>
+                  </CardBody>
+                </Card>
+              </div>
+            </div>
+          </div>
+
+          <div id="instrucciones" className="relative py-6 md:mt-16 px-4 lg:px-16 mx-4 w-full">
+            <h2 className="text-3xl font-bold tracking-tighter lg:text-2xl xl:text-5xl md:px-16 mb-8">
+              Instrucciones
+            </h2>
+            <div className="flex flex-wrap justify-center">
+              <div className="w-full lg:w-2/3 px-4 mb-4">
                 <Accordion selectionMode="multiple">
                   <AccordionItem
                     key="1"
-                    aria-label="Tesis o Proyecto de Grado"
-                    startContent={
-                      <Avatar
-                        isBordered
-                        color="primary"
-                        radius="lg"
-                        src="https://i.pravatar.cc/150?u=a042581f4e29026024d"
-                      />
-                    }
-                    title="Tesis/Proyecto Grado"
-                  >
-                    <p className="text-sm">
-                      Necesitas proponer un tema. Revisa proyectos anteriores en
-                      la pestaña "Herramienta de Proyectos Pasados".
-                    </p>
-                  </AccordionItem>
-                  <AccordionItem
-                    key="2"
-                    aria-label="Graduación por Excelencia"
-                    startContent={
-                      <Avatar
-                        isBordered
-                        color="success"
-                        radius="lg"
-                        src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
-                      />
-                    }
-                    title="Graduación Excelencia"
-                  >
-                    <p className="text-sm">
-                      Necesitas tener notas por encima de 90, sin ninguna
-                      materia reprobada o abandonada en toda la malla
-                      curricular.
-                    </p>
-                  </AccordionItem>
-                </Accordion>
-              </div>
-              <div className="w-full lg:w-1/3 px-4">
-                <Accordion selectionMode="multiple">
-                  <AccordionItem
-                    key="3"
-                    aria-label="Trabajo Dirigido"
-                    startContent={
-                      <Avatar
-                        isBordered
-                        color="warning"
-                        radius="lg"
-                        src="https://i.pravatar.cc/150?u=a04258114e29026702d"
-                      />
-                    }
-                    title="Trabajo Dirigido"
-                  >
-                    <p className="text-sm">
-                      Necesitarás hacer un trabajo específico en una empresa,
-                      esto se coordina junto a un tutor de la empresa a la que
-                      harás el trabajo, al igual que un tutor para la UCB que
-                      vea tu progreso. Tienes que cumplir un objetivo dentro de
-                      la empresa para aprobar exitosamente.
-                    </p>
-                  </AccordionItem>
-                  <AccordionItem
-                    key="4"
                     aria-label="Requisitos Académicos"
-                    startContent={
-                      <Avatar
-                        isBordered
-                        color="primary"
-                        radius="lg"
-                        src="https://i.pravatar.cc/150?u=a042581f4e29026024d"
-                      />
-                    }
+                    startContent={<div className={`border-2 p-2 rounded-lg border-primary-500`}>
+                    <ClipboardList size={24} />
+                  </div>}
                     title="Requisitos Académicos"
                   >
                     <ul className="list-disc list-inside text-left text-sm lg:text-sm">
@@ -146,21 +162,12 @@ export const HeroSection = () => {
                       </li>
                     </ul>
                   </AccordionItem>
-                </Accordion>
-              </div>
-              <div className="w-full lg:w-1/3 px-4">
-                <Accordion selectionMode="multiple">
                   <AccordionItem
-                    key="5"
+                    key="2"
                     aria-label="Proceso de Inscripción"
-                    startContent={
-                      <Avatar
-                        isBordered
-                        color="success"
-                        radius="lg"
-                        src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
-                      />
-                    }
+                    startContent={<div className={`border-2 p-2 rounded-lg border-warning`}>
+                    <MailPlus size={24} />
+                  </div>}
                     title="Proceso Inscripción"
                   >
                     <ol className="list-decimal list-inside text-left text-sm lg:text-sm">
@@ -185,16 +192,11 @@ export const HeroSection = () => {
                     </ol>
                   </AccordionItem>
                   <AccordionItem
-                    key="6"
+                    key="3"
                     aria-label="Fechas Importantes"
-                    startContent={
-                      <Avatar
-                        isBordered
-                        color="warning"
-                        radius="lg"
-                        src="https://i.pravatar.cc/150?u=a04258114e29026702d"
-                      />
-                    }
+                    startContent={<div className={`border-2 p-2 rounded-lg border-primary-500`}>
+                    <  CalendarClock size={24} />
+                  </div>}
                     title="Fechas Importantes"
                   >
                     <ul className="list-disc list-inside text-left text-sm lg:text-sm">
@@ -214,6 +216,7 @@ export const HeroSection = () => {
               </div>
             </div>
           </div>
+          <div id="noticias"></div>
         </div>
       </div>
     </>
