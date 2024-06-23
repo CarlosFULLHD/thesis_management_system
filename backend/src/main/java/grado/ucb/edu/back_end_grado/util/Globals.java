65aef936baa517ad5f77c103c1cf2023e59533ba
CarlosFULLHD <cninareynaga@gmail.com>
2024-06-23 19:43:05 -0400

    Github push

--- backend/src/main/java/grado/ucb/edu/back_end_grado/util/Globals.java
+++ backend/src/main/java/grado/ucb/edu/back_end_grado/util/Globals.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 public class Globals {
     public static final String apiVersion = "api/v1/";
 
-    public static final String reCaptchaSiteSecret = "6LchpbIpAAAAALil_-76o5mL_8nklA0tuSZZrvMa";
+    public static final String reCaptchaSiteSecret = "asd";
 
     public static final String[] httpOkStatus = HttpStatus.OK.toString().split(" "); // Status 200
 
@@ -15,7 +15,9 @@ public class Globals {
     public static final String[] httpBadRequest = HttpStatus.BAD_REQUEST.toString().split(" "); /// Status 400
     public static final String[] httpNotFoundStatus = HttpStatus.NOT_FOUND.toString().split(" "); // Status 404
 
-    public static final String[] httpMethodNowAllowed = HttpStatus.METHOD_NOT_ALLOWED.toString().split(" "); // Status 405
-    public static final String[] httpInternalServerErrorStatus = HttpStatus.INTERNAL_SERVER_ERROR.toString().split(" "); // Status 500
+    public static final String[] httpMethodNowAllowed = HttpStatus.METHOD_NOT_ALLOWED.toString().split(" "); // Status
+                                                                                                             // 405
+    public static final String[] httpInternalServerErrorStatus = HttpStatus.INTERNAL_SERVER_ERROR.toString().split(" "); // Status
+                                                                                                                         // 500
 
 }
